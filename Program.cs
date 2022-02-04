58df582a17ebef2bf2414fde4c6e03f71584166d
Davi Borges <davifelipe_300@hotmail.com>
2022-02-04 17:56:03 -0300

    Programa interrompido com instrução break

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExemploPraticoInstrucaoBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao;
+            
+            while (true)
+            {
+                while (true)
+                {
+                    Console.Write("Deseja continuar? [s] sim ou [n] não: ");
+                    opcao = Console.ReadLine();
+
+                    if (opcao == "n")
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Laço externo");    
+            }
+            
+        }
+    }
+}
